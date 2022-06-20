689fc46ac7eedf1a2d35dc28d2ae4d1b56e4f77c
Greg Brooks <gregorybrooks@gmail.com>
2022-06-19 22:06:35 -0400

    Bump version

--- runit.sh
+++ runit.sh
@@ -1,5 +1,5 @@
 set -v
-java -jar target/BetterQueryBuilderTaskNounPhrases-2.0.0.jar $eventExtractorFileLocation/$INPUTFILE $MODE $queryFileLocation/$QUERYFILE $OUT_LANG $HOME/programfiles $PHASE $englishIndexLocation $logFileLocation $galagoLocation $qrelFile $targetIndexLocation
+java -jar target/BetterQueryBuilderTaskNounPhrases-2.1.0.jar $eventExtractorFileLocation/$INPUTFILE $MODE $queryFileLocation/$QUERYFILE $OUT_LANG $HOME/programfiles $PHASE $englishIndexLocation $logFileLocation $galagoLocation $qrelFile $targetIndexLocation
 set +e
 chmod -f a+rw $queryFileLocation/${QUERYFILE}*
 exit 0
